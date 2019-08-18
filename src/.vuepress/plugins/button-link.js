5a4f65a09ed461491b5c23e47708c5d74d799d1a
Nils Knappmeier <github@knappi.org>
2019-08-18 16:07:00 +0200

    fix eslint warnings

--- src/.vuepress/plugins/button-link.js
+++ src/.vuepress/plugins/button-link.js
@@ -7,7 +7,7 @@ function buttonLink() {
     name: "button link",
     extendMarkdown(md) {
       // Add a post-processing rule that gives all links with a text like "Learn more:" the css-class "learn-more-button"
-      md.inline.ruler2.push("button_link", function learnMore(state, silent) {
+      md.inline.ruler2.push("button_link", function learnMore(state) {
         if (state.src.startsWith("!button[")) {
           const token = state.tokens[1];
           if (token && token.type === "link_open") {
