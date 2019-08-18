da65de413869ab6f0b8a5db2438e15fe46b8f9e9
Nils Knappmeier <github@knappi.org>
2019-08-18 16:05:32 +0200

    chore: fix eslint config for test-files (**/*.test.js), bump dependency versions
    - see https://github.com/eslint/eslint/issues/12114 for details
      on the required eslint-upgrade

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,29 @@
 module.exports = {
   extends: ["eslint:recommended", "plugin:vue/recommended", "plugin:jest/recommended", "prettier", "prettier/vue"],
+  plugins: ["jest"],
   parser: "vue-eslint-parser",
   parserOptions: {
     parser: "babel-eslint",
     ecmaVersion: 8,
     sourceType: "module"
-  }
+  },
+  env: {
+    "jest/globals": false,
+    jest: false
+  },
+  overrides: [
+    {
+      files: ["**/*.test.js"],
+      env: {
+        "jest/globals": true,
+        jest: true
+      }
+    },
+    {
+      files: ["src/.vuepress/plugins/**/*.js"],
+      env: {
+        node: true
+      }
+    }
+  ]
 };
